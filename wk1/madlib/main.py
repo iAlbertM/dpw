23aca5e51272ca7223e3a7968b69085febf9b803
Albert Martinez <albertm@fullsail.edu>
2016-06-04 21:13:24 -0700

    initial upload of first assignment: MadLib
    my first commit shows the story that will be created when all is
    finished.

---
+++ wk1/madlib/main.py
@@ -0,0 +1,24 @@
+'''
+Intro to Python: MadLibsss
+author: Albert Martinez
+class:Design Patterns for Web Programming DPW-1606
+instructor: Rebecca Carroll
+'''
+
+#MadLib
+madlib = '''
+The Way it Works at Work
+So you are now, what {age} years old, right? You think you know how it works in the {occupation} world
+just remember these few tips:
+When you greet a coworker in the breakroom say, "Hey, working hard or {adverb} working?" This shows them you are " The {nickname} ."
+Make friends with the cleaning staff, they always know where the {object_plural} are.
+Be nice to your boss. Kiss {body_part} to stay on her good side.
+If your boss ever asks you to guess her age, REMEMBER:
+If she looks {old_term} , shave off {large_number} from your original guess and pray she doesn't {verb} you.
+If she looks young, repeat the same step as before.
+Lastly, if ever asked to stay overtime and work overtime without getting overtime because it's "volunteer work",
+never say, " {angry_phrase} " instead say, " {nice_phrase} "
+Good luck,
+Love {annoying_person}
+xoxo
+'''
