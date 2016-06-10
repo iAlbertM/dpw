c57f1a0fc6b00d962a0fdbdfabf7c5a362926990
Albert Martinez <albertm@fullsail.edu>
2016-06-09 20:15:05 -0700

    added initial page class containing html for the simple form

--- wk2/simple-login/main.py
+++ wk2/simple-login/main.py
@@ -8,7 +8,38 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self): # function to start it all up
-        self.response.write("Hello World! It is I, Albert M!")
+        page = '''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+        	<meta charset="UTF-8">
+        	<title>Albert Martinez | Simple Form | DPW-1606 WDDBS Full Sail University</title>
+        </head>
+        <body>
+        	<form method="get">
+        		<p class="input-field">
+        			<label for="name">Name</label>
+        			<input type="text" placeholder="Name" id="name" name="name">
+        		<p>
+        		<p class="input-field">
+        			<label for="email">Email</label>
+        			<input type="email" placeholder="Email" id="email" name="email">
+        		<p>
+        		<p class="input-field">
+        			<label for="gender">Gender</label>
+        			<p class="input-field">
+        				<input type="radio" name="gender" value="Male">
+        			</p>
+        			<p class="input-field">
+        				<input type="radio" name="gender" value="Female">
+        			</p>
+        		<p>
+        		<input type="submit value="submit" />
+        	</form>
+        </body>
+        </html>
+                '''
+        self.response.write(page)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
